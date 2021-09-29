13026ee1abc57ba8a190b86bb668f6234703b4e4
nobuhiko shimaya <shimayanobuhiko@gmail.com>
2021-09-29 11:53:33 +0900

    +-の数値作成

--- src/Counter.js
+++ src/Counter.js
@@ -1,12 +1,31 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+    };
+  }
+
+  increment = () => {
+    this.setState({
+      count: this.state.count + 1,
+    });
+  };
+
+  decrement = () => {
+    this.setState({
+      count: this.state.count - 1,
+    });
+  };
+
   render() {
     return (
       <div>
-        <p>Count : </p>
-        <button>+</button>
-        <button>-</button>
+        <p>Count : {this.state.count}</p>
+        <button onClick={this.increment}>+</button>
+        <button onClick={this.decrement}>-</button>
       </div>
     );
   }
